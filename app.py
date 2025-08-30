0e8643bfde79e805704a0be0aed2c8b047f4f9f0
dinethwick <dinethwick23@gmail.com>
2025-08-30 16:20:57 +0930

    Added pages and created navigation bar. We now have individual  pages for students, educators as well as a user guide + about page

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
 import streamlit as st
 
-st.title("GEN AI POLICY dashboard")
-st.write("Policy categories go here")
-st.slider("pick a number", 0, 100)
+# --- PAGE SETUP ---
+user_guide_page = st.Page (
+    page="pages/user_guide.py",
+    title="User Guide for this application",
+    #icon=":material/account_circle",
+)
+
+students_page = st.Page (
+    page="pages/students.py",
+    title="Policies for students"
+)
+educators_page = st.Page (
+    page="pages/educators.py",
+    title="Policies for educators"
+)
+about_page = st.Page (
+    page="pages/about.py",
+    title="About this project",
+    default=True                                   # this is the home page for now
+)
+
+# -- Navigation Setup -- 
+pg = st.navigation(pages=[students_page, educators_page, user_guide_page, about_page])
+pg.run()
---
+++ pages/about.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("About this project")
+st.write("This project is aimed at creating a unified policy databse for Australasian Universities.")
